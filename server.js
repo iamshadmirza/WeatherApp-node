9b692e0c15676cbcc8a62ef69d98b6f087f769c7
iamshadmirza <md.shad1@yahoo.com>
2019-03-28 07:55:35 +0530

    handlebars partials & helpers

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const hbs = require('hbs');
 
+hbs.registerPartials(__dirname + '/views/partials');
 var app = express();
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('currentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
 
 app.get('/', (req, res) => {
     res.send('Hello Express!!');
@@ -12,7 +16,7 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Welcome to about us page'
     });
 })
 
