034d0dfb14b062be26bfc43b9b02ce102fb43198
iamshadmirza <md.shad1@yahoo.com>
2019-03-29 23:02:03 +0530

    setup start script and heruku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 hbs.registerPartials(__dirname + '/views/partials');
+const port = process.env.PORT || 3000;
 var app = express();
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
@@ -24,4 +25,6 @@ app.get('/about', (req, res) => {
     });
 })
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
