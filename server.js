ad0b919f55830e4f1da297983bf967e778d51f06
iamshadmirza <md.shad1@yahoo.com>
2019-03-29 21:54:41 +0530

    express middleware

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 hbs.registerHelper('currentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', (text) => text.toUpperCase());
-
+app.use((req, res, next) => {
+    var date = new Date().toDateString();
+    console.log(`${date}: ${req.method} ${req.url}`);
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello Express!!');
