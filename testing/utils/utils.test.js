e8f894b42f7fc95a01653340cd56ab75ea7f7fbd
iamshadmirza <md.shad1@yahoo.com>
2019-03-30 07:06:24 +0530

    testing with expect

--- testing/utils/utils.test.js
+++ testing/utils/utils.test.js
@@ -1,17 +1,19 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two number', () => {
     var res = utils.add(33, 11);
-
-    if (res !== 44) {
-        throw new Error(`Expected 44 but got ${res}`);
-    }
+    expect(res).toBe(44);
+    // if (res !== 44) {
+    //     throw new Error(`Expected 44 but got ${res}`);
+    // }
 });
 
 it('should add two number', () => {
     var res = utils.add(11, 11);
-
-    if (res !== 23) { // fail test
-        throw new Error(`Expected 23 but got ${res}`);
-    }
+    expect(res).toBe(22);
 });
+
+it('should include number in an array', () => {
+    expect([2, 4, 6]).toInclude(4);
+})
