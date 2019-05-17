973fdd51593f894d07db2a0c715ffe2ac67a8215
iamshadmirza <md.shad1@yahoo.com>
2019-05-17 15:48:46 +0530

    access_token added

---
+++ src/utils/access_token.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mapbox_token: "pk.eyJ1IjoiaWFtc2hhZG1pcnphIiwiYSI6ImNqbmg1MGF5aDA5ZGQzcHRsdndjZGswZ28ifQ.hW4SwPQTKAbZNBAHQ7aQyg",
+    darksky_token: "9eed8fb0b746aa662996d97528b32333"
+}
